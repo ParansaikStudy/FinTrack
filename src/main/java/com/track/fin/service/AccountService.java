ca9960dd7b1475fef105fe3c7ed55ed3ad31cba7
paransaik <jty419@naver.com>
2025-04-12 12:30:33 +0900

    refactor: TransactionService, AccountService 코드 컨벤션 수정
    -. class 시작 후 final 필드 추가 시 라인 개행을 한다.
    -. 메소드 간의 간격은 개행 하나로 한다.
    -. return 이후에 라인 개행을 삭제 한다.
    -. 불필요한 주석은 삭제 한다.

--- src/main/java/com/track/fin/service/AccountService.java
+++ src/main/java/com/track/fin/service/AccountService.java
@@ -22,50 +22,34 @@ import static com.track.fin.type.ErrorCode.USER_NOT_FOUND;
 @Service
 @RequiredArgsConstructor
 public class AccountService {
+
     private final AccountRepository accountRepository;
     private final AccountUserRepository accountUserRepository;
 
-    /**
-     * 사용자가 있는지 조회
-     * 계좌에 번호를 생성하고
-     * 계좌를 저장하고, 그 정보를 넘긴다.
-     */
     @Transactional
     public AccountDto createAccount(Long userId, Long initialBalance) {
         AccountUser accountUser = accountUserRepository.findById(userId)
                 .orElseThrow(() -> new AccountException(USER_NOT_FOUND));
 
         validateCreateAccount(accountUser);
-        //가장 최근에 생성되 account를 가져오고 그 account에서 accountNumber를 받아서 문자를 숫자로 변환하고 다시 문자열로 바꿔준다.
         String newAccountNumber = accountRepository.findFirstByOrderByIdDesc()
                 .map(account -> (Integer.parseInt(account.getAccountNumber())) + 1 + " ")
-                // 계좌가 하나도 없으면
                 .orElse("1000000000");
 
-        //신규 계좌 저장
-
-       return AccountDto.fromEntity(
-               accountRepository.save(Account.builder()
-                       .accountUser(accountUser)
-                       .accountStatus(AccountStatus.IN_USE)
-                       .accountNumber(newAccountNumber)
-                       .balance(initialBalance)
-                       .registeredAt(LocalDateTime.now())
-                       .build())
-       );
-
-
-    }
-
-    private void validateCreateAccount(AccountUser accountUser) {
-        if(accountRepository.countByAccountUser(accountUser) == 10) {
-            throw new AccountException(ErrorCode.MAX_ACCOUNT_PER_USER_10);
-        }
+        return AccountDto.fromEntity(
+                accountRepository.save(Account.builder()
+                        .accountUser(accountUser)
+                        .accountStatus(AccountStatus.IN_USE)
+                        .accountNumber(newAccountNumber)
+                        .balance(initialBalance)
+                        .registeredAt(LocalDateTime.now())
+                        .build())
+        );
     }
 
     @Transactional
     public Account getAccount(Long id) {
-        if(id < 0){
+        if (id < 0) {
             throw new RuntimeException("Minus");
         }
         return accountRepository.findById(id).get();
@@ -83,24 +67,7 @@ public class AccountService {
         account.setAccountStatus(AccountStatus.UNREGISTERED);
         account.setUnregisteredAt(LocalDateTime.now());
 
-        accountRepository.save(account);
-
-        return AccountDto.fromEntity(account);
-
-    }
-
-    
-
-    private void validateDeleteAccount(AccountUser accountUser, Account account) {
-        if(!Objects.equals(accountUser.getId(), account.getAccountUser().getId())) {
-             throw  new AccountException(ErrorCode.USER_ACCOUNT_UNMATCH);
-        }
-        if(account.getAccountStatus() == AccountStatus.UNREGISTERED) {
-            throw new AccountException(ErrorCode.ACCOUNT_ALREADY_UNREGISTERED);
-        }
-        if(account.getBalance() > 0) {
-            throw new AccountException(ErrorCode.BALANCE_NOT_EMPTY);
-        }
+        return AccountDto.fromEntity(accountRepository.save(account));
     }
 
     @Transactional
@@ -115,4 +82,23 @@ public class AccountService {
                 .map(AccountDto::fromEntity)
                 .collect(Collectors.toList());
     }
+
+    private void validateCreateAccount(AccountUser accountUser) {
+        if (accountRepository.countByAccountUser(accountUser) == 10) {
+            throw new AccountException(ErrorCode.MAX_ACCOUNT_PER_USER_10);
+        }
+    }
+
+    private void validateDeleteAccount(AccountUser accountUser, Account account) {
+        if (!Objects.equals(accountUser.getId(), account.getAccountUser().getId())) {
+            throw new AccountException(ErrorCode.USER_ACCOUNT_UNMATCH);
+        }
+        if (account.getAccountStatus() == AccountStatus.UNREGISTERED) {
+            throw new AccountException(ErrorCode.ACCOUNT_ALREADY_UNREGISTERED);
+        }
+        if (account.getBalance() > 0) {
+            throw new AccountException(ErrorCode.BALANCE_NOT_EMPTY);
+        }
+    }
+
 }
