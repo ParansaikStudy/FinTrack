a33f6356e36094ac67b45722a263ec2d34855794
WonJin <try3982@kakao.com>
2025-05-08 20:38:35 +0900

    refactor: Enum 사용시 모든 Enum도 import로 사용하도록 변경

--- src/main/java/com/track/fin/service/AccountService.java
+++ src/main/java/com/track/fin/service/AccountService.java
@@ -7,9 +7,8 @@ import com.track.fin.exception.AccountException;
 import com.track.fin.record.AccountRecord;
 import com.track.fin.repository.AccountRepository;
 import com.track.fin.repository.UserRepository;
-import com.track.fin.type.AccountStatus;
+
 import com.track.fin.type.AccountType;
-import com.track.fin.type.ErrorCode;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,7 +18,8 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
-import static com.track.fin.type.ErrorCode.USER_NOT_FOUND;
+import static com.track.fin.type.ErrorCode.*;
+import static com.track.fin.type.AccountStatus.*;
 
 @Service
 @RequiredArgsConstructor
@@ -39,7 +39,7 @@ public class AccountService {
         String newAccountNumber = generateUniqueAccountNumber();
         Account account = accountRepository.save(Account.builder()
                 .user(user)
-                .accountStatus(AccountStatus.ACTIVE)
+                .accountStatus(ACTIVE)
                 .accountNumber(newAccountNumber)
                 .balance(initialBalance)
                 .accountType(accountType)
@@ -59,15 +59,12 @@ public class AccountService {
     @Transactional
     public Account getAccountByNumber(String accountNumber) {
         return accountRepository.findByAccountNumber(accountNumber)
-                .orElseThrow(() -> new AccountException(ErrorCode.ACCOUNT_NOT_FOUND));
+                .orElseThrow(() -> new AccountException(ACCOUNT_NOT_FOUND));
     }
 
-
-    // TODO: 특정 계좌 담보 대출 가능한 금액 출력
     public BigDecimal getAccountCollateralRate(Long userId, Long accountId) {
         User user = userService.get(userId);
         Account account = this.getAccount(accountId);
-        // 회원 등급에 따른 %
         return user.getGrade().getGradeType().getCollateralRate().multiply(BigDecimal.valueOf(account.getBalance()));
     }
 
@@ -83,7 +80,6 @@ public class AccountService {
                 .orElseThrow(() -> new AccountException(USER_NOT_FOUND));
 
         validateDeleteAccount(user, account);
-
         account.afterLoan();
 
         return AccountRecord.from(accountRepository.save(account));
@@ -93,8 +89,7 @@ public class AccountService {
     public List<AccountDto> getAccountsByuserId(Long userId) {
         User user = userService.get(userId);
 
-        List<Account> accounts = accountRepository
-                .findByUser(user);
+        List<Account> accounts = accountRepository.findByUser(user);
 
         return accounts.stream()
                 .map(AccountRecord::from)
@@ -103,45 +98,29 @@ public class AccountService {
 
     private void validateCreateAccount(User user) {
         if (accountRepository.countByUser(user) == 10) {
-            throw new AccountException(ErrorCode.MAX_ACCOUNT_PER_USER_10);
+            throw new AccountException(MAX_ACCOUNT_PER_USER_10);
         }
     }
 
     private void validateDeleteAccount(User user, Account account) {
         if (!Objects.equals(user.getId(), account.getUser().getId())) {
-            throw new AccountException(ErrorCode.USER_ACCOUNT_UNMATCH);
+            throw new AccountException(USER_ACCOUNT_UNMATCH);
         }
-        if (account.getAccountStatus() == AccountStatus.CLOSED) {
-            throw new AccountException(ErrorCode.ACCOUNT_ALREADY_UNREGISTERED);
+        if (account.getAccountStatus() == CLOSED) {
+            throw new AccountException(ACCOUNT_ALREADY_UNREGISTERED);
         }
         if (account.getBalance() > 0) {
-            throw new AccountException(ErrorCode.BALANCE_NOT_EMPTY);
+            throw new AccountException(BALANCE_NOT_EMPTY);
         }
     }
 
-
-    // 정수 숫자를 사용 하면 X
-    // 1. 재사용성 x
-    // 2. 계산 할 때 오타 -> 컴파일 시점에서 오류가 안생겨서 에러 잡기 힘듦
-    // 3. 코드 리뷰할 때 이
     private void validateInitialBalance(Long initialBalance, AccountType accountType) {
         if (accountType.getBalance().equals(initialBalance)) {
-            throw new AccountException(ErrorCode.INSUFFICIENT_INITIAL_BALANCE);
+            throw new AccountException(INSUFFICIENT_INITIAL_BALANCE);
         }
     }
 
-    // 개발 잘하시는 분들은 enum을 기가막히게 쓰십니다
-    // 그래서 enum 공부 많이 하세요
-    // review 용
     private Double calculatorRate(AccountType accountType) {
-//        double rate = 0.0D;
-//        if (accountType.equals(AccountType.SAVINGS)) {
-//            rate = 10000000 * 0.1;
-//        } else if (accountType.equals(AccountType.DEPOSIT.DEPOSIT)) {
-//            rate = 10000 * 0.2;
-//        } else {
-//            rate = 0;
-//        }
         return accountType.getBalance() * accountType.getRate();
     }
 
